44a6b11d43707e7f75522f683563fe37e78c0829
Pavel Blokhin <blokhin321@gmail.com>
2017-09-09 23:29:44 +0500

    Lab

--- 3/3_1/3_1.cpp
+++ 3/3_1/3_1.cpp
@@ -10,7 +10,27 @@ using namespace std;
 
 int main()
 {
+	freopen("INPUT.TXT", "r", stdin);
+	freopen("OUTPUT.TXT", "w", stdout);
 
-	//Sort without sort. Why?
+	int n = (int)1e6;
+	int *a = new int[n];
+	int k = 0;
+	while (cin >> a[k])
+	{
+		k++;
+	}
+	for (int j = 0; j < k; j++) {
+		for (int i = j; i < k; i++)
+		{
+			if (a[i] > a[i + 1])
+			{
+				swap(a[i], a[i + 1]);
+			}
+		}
+	}
+	for (int i = 0; i < k; i++)
+		cout << a[i] << " ";
+
 	return 0;
 }
